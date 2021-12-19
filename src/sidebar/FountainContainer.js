d3ae4df44a30c82460e6c977fc7c786d0e19d9a3
Domiziano Scarcelli <scarcelli.d@outlook.com>
2021-12-19 14:51:43 +0100

    Fetch data from api and displays it in the sidebar

--- src/sidebar/FountainContainer.js
+++ src/sidebar/FountainContainer.js
@@ -1,18 +1,24 @@
 import "./Fountain.css";
 import { FountainItem } from "./FountainItem";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const FountainContainer = (props) => {
+	const [fountainList, setFountainList] = useState([]);
+
+	useEffect(() => console.log(fountainList), [fountainList]);
+
 	useEffect(() => {
 		fetch(`https://fontanelle-api.test.sapienzaapps.it/fountains/?latitude=${props.location.lat}&longitude=${props.location.lng}&radius=100`)
 			.then((response) => response.json())
-			.then((data) => console.log(data));
+			.then((data) => {
+				setFountainList(data);
+			});
 	}, []);
 	return (
 		<div className="fountainContainer">
-			<FountainItem address="Via di casal selce, 294" />
-			<FountainItem address="Via di casal selce, 294" />
-			<FountainItem address="Via di casal selce, 294" />
+			{fountainList.map((data) => {
+				return <FountainItem address={data.ID} />;
+			})}
 		</div>
 	);
 };
